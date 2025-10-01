ff2e6d93ae2c6a7f97b978997993f47422d298a6
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-10-01 20:49:26 -0300

    feat: create getOperatorIndex
    It returns the index of the mathematical
    operators if it exists; returns -1 otherwise.

---
+++ src/scripts/parser/getOperatorIndex.ts
@@ -0,0 +1,17 @@
+const getOperatorIndex = (expression: string): number => {
+	for (let i in expression.split("")) {
+		const isOperator =
+			expression[i] === "*" ||
+			expression[i] === "/" ||
+			expression[i] === "+" ||
+			expression[i] === "-";
+
+		if (isOperator) {
+			return Number(i);
+		}
+	}
+
+	return -1;
+};
+
+export default getOperatorIndex;
