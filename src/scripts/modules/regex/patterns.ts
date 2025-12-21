e054714e8d5a15f81b05f7ab370233ee62a61de3
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-12-21 12:35:02 -0300

    feat: create regex for expressions that starts with operator

--- src/scripts/modules/regex/patterns.ts
+++ src/scripts/modules/regex/patterns.ts
@@ -11,3 +11,13 @@
  */
 export const VALID_EXPRESSION_REGEX =
 	/^[\+\-]?(?<firstOperand>[0-9]+(?:\.[0-9]+)?)(?:(?<operator>[\+\-\*\/])(?<secondOperand>[0-9]+(?:\.[0-9]+)?))+$/;
+
+/**
+ * Valid examples:
+ *
+ * -12+5
+ *
+ * +0.5/10.5
+ *
+ */
+export const EXPRESSION_START_WITH_OPERATOR_REGEX = /^[\+\-]/;
