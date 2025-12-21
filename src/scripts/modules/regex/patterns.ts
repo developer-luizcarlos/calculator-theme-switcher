2bbcf65a145516dc4950efc201c9ee87f6b1093a
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-12-21 10:37:32 -0300

    feat: create regex pattern for a valid math expression

---
+++ src/scripts/modules/regex/patterns.ts
@@ -0,0 +1,13 @@
+/**
+ * Valid expression examples:
+ *
+ * 12+5-1.420
+ *
+ * 0.5*100
+ *
+ * 4/2*8
+ *
+ * 0.125/12-33*4.2
+ */
+export const VALID_EXPRESSION_REGEX =
+	/^[\+\-]?(?<firstOperand>[0-9]+(?:\.[0-9]+)?)(?:(?<operator>[\+\-\*\/])(?<secondOperand>[0-9]+(?:\.[0-9]+)?))+$/;
