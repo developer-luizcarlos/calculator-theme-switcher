b4fd8897d688a0475361d9628fb65500c2f8772a
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-12-22 14:53:10 -0300

    refact: move screen functions from handlers to separeted file.

--- dist/dom/screen.js
+++ dist/dom/screen.js
@@ -1,7 +1,70 @@
+import { parser } from "../modules/parser/parser.js";
+import { EXPRESSION_LAST_CHAR_IS_OPERATOR_REGEX } from "../modules/regex/patterns.js";
 import { $screen } from "./elements.js";
 export const SCREEN_VALUE_LENGTH_LIMIT = 10;
+export function deleteLastDigit() {
+    const screenValue = getScreenValue();
+    if (screenValue === "0") {
+        return;
+    }
+    if (screenValue.length === 1) {
+        setScreenValue("0");
+    }
+    else {
+        const slicedScreenValue = screenValue.slice(0, -1);
+        setScreenValue(slicedScreenValue);
+    }
+}
+export function displayResult() {
+    const expression = getScreenValue();
+    const evaluatedExpression = parser(expression);
+    if (evaluatedExpression === expression) {
+        return;
+    }
+    setScreenValue(evaluatedExpression);
+}
 export function getScreenValue() {
     return $screen.textContent;
+}
+export function insertOperatorDigit(digit) {
+    const screenValue = getScreenValue();
+    const screenValueLength = screenValue.length;
+    const isLastCharAnOperator = EXPRESSION_LAST_CHAR_IS_OPERATOR_REGEX.test(screenValue);
+    if (isLastCharAnOperator) {
+        return;
+    }
+    if (screenValueLength < SCREEN_VALUE_LENGTH_LIMIT) {
+        if (screenValue === "0") {
+            if (digit === "+" || digit === "-") {
+                setScreenValue(digit);
+            }
+            else {
+                setScreenValue(screenValue + digit);
+            }
+        }
+        else {
+            setScreenValue(screenValue + digit);
+        }
+    }
+}
+export function insertNumericDigit(digit) {
+    const screenValue = getScreenValue();
+    const screenValueLength = screenValue.length;
+    if (screenValueLength < SCREEN_VALUE_LENGTH_LIMIT) {
+        if (screenValue === "0") {
+            setScreenValue(digit);
+        }
+        else {
+            setScreenValue(screenValue + digit);
+        }
+    }
+}
+export function reset() {
+    const screenValue = getScreenValue();
+    if (screenValue === "0") {
+        return;
+    }
+    setScreenValue("0");
 }
 export function setScreenValue(value) {
     $screen.textContent = value;
--- src/scripts/dom/screen.ts
+++ src/scripts/dom/screen.ts
@@ -1,9 +1,88 @@
+import {parser} from "../modules/parser/parser.ts";
+import {EXPRESSION_LAST_CHAR_IS_OPERATOR_REGEX} from "../modules/regex/patterns.ts";
 import {$screen} from "./elements.ts";
 
 export const SCREEN_VALUE_LENGTH_LIMIT = 10;
 
+export function deleteLastDigit() {
+	const screenValue = getScreenValue();
+
+	if (screenValue === "0") {
+		return;
+	}
+
+	if (screenValue.length === 1) {
+		setScreenValue("0");
+	} else {
+		const slicedScreenValue = screenValue.slice(0, -1);
+
+		setScreenValue(slicedScreenValue);
+	}
+}
+
+export function displayResult() {
+	const expression = getScreenValue();
+
+	const evaluatedExpression = parser(expression);
+
+	if (evaluatedExpression === expression) {
+		return;
+	}
+
+	setScreenValue(evaluatedExpression);
+}
+
 export function getScreenValue(): string {
 	return $screen!.textContent!;
+}
+
+export function insertOperatorDigit(digit: string): void {
+	const screenValue = getScreenValue();
+
+	const screenValueLength = screenValue.length;
+
+	const isLastCharAnOperator =
+		EXPRESSION_LAST_CHAR_IS_OPERATOR_REGEX.test(screenValue);
+
+	if (isLastCharAnOperator) {
+		return;
+	}
+
+	if (screenValueLength < SCREEN_VALUE_LENGTH_LIMIT) {
+		if (screenValue === "0") {
+			if (digit === "+" || digit === "-") {
+				setScreenValue(digit);
+			} else {
+				setScreenValue(screenValue + digit);
+			}
+		} else {
+			setScreenValue(screenValue + digit);
+		}
+	}
+}
+
+export function insertNumericDigit(digit: string): void {
+	const screenValue = getScreenValue();
+
+	const screenValueLength = screenValue.length;
+
+	if (screenValueLength < SCREEN_VALUE_LENGTH_LIMIT) {
+		if (screenValue === "0") {
+			setScreenValue(digit);
+		} else {
+			setScreenValue(screenValue + digit);
+		}
+	}
+}
+
+export function reset(): void {
+	const screenValue = getScreenValue();
+
+	if (screenValue === "0") {
+		return;
+	}
+
+	setScreenValue("0");
 }
 
 export function setScreenValue(value: string): void {
